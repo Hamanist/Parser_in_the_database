77a784b04decf5218d06cd46656eee06abd37e0b
Hamanist <hamanist@gmail.com>
2024-01-31 23:03:45 +0300

    add code

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
-# Получить данные о работодателях и их вакансиях с сайта hh.ru. Для этого используйте публичный API hh.ru и библиотеку
-# requests
-# .
-# Выбрать не менее 10 интересных вам компаний, от которых вы будете получать данные о вакансиях по API.
-# Спроектировать таблицы в БД PostgreSQL для хранения полученных данных о работодателях и их вакансиях.
-# Для работы с БД используйте библиотеку psycopg2
-# .
-# Реализовать код, который заполняет созданные в БД PostgreSQL таблицы данными о работодателях и их вакансиях.
-# Создать класс
-# DBManager для работы с данными в БД.
 from manager.bd_manager import DBManager
 from parse_class.parse import Parse
 
-#Parse().get_data()
 
-# for i in Parse().get_data():
-#     print(i)
-DBManager().connect_bd()
+def main():
+    Parse().get_data()
+    DBManager().connect_bd()
+    try:
+        while True:
+            user = int(input("""
+        Что вам нужно:
+        Получить список всех компаний и количество вакансий у каждой компании - нажмите 1
+        Получить список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию - нажмите 2
+        Получает среднюю зарплату по вакансиям - нажмите 3
+        Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям - нажмите 4
+        Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например "python" - нажмите 5
+        Выход - нажмите 0\n--->
+        """))
 
-# String Data Right Truncation: value too long for type character varying(50)
-#Unexpected argument
+            if user == 1:
+                report = DBManager().get_companies_and_vacancies_count()
+                for i in report:
+                    print(*i)
+            elif user == 2:
+                report = DBManager().get_all_vacancies()
+                for i in report:
+                    print(*i)
+            elif user == 3:
+                report = DBManager().get_avg_salary()
+                for i in report:
+                    print(*i)
+            elif user == 4:
+                report = DBManager().get_vacancies_with_higher_salary()
+                for i in report:
+                    print(*i)
+            elif user == 5:
+                word = input("Введите слово")
+                report = DBManager().get_vacancies_with_keyword(word)
+                for i in report:
+                    print(*i)
+            else:
+                print('Не правильно ввели данные')
+    except ValueError:
+        print("Введите число")
+
+if __name__ == '__main__':
+    main()
